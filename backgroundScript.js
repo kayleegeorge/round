71b6a76780715725ab254ab33fb2ee8e0a31a732
suzannenie <nie.suzanne01@gmail.com>
2020-07-11 13:49:41 -0700

    basic

--- backgroundScript.js
+++ backgroundScript.js
@@ -1,7 +1,19 @@
 //backgroundScript.js
-chrome.browserAction.onClicked.addListener(
-function(tab) {
-chrome.tabs.executeScript(tab.id, {
-"file": "contentScript.js"
+// content script accesses page but background script does useful things
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    
+    chrome.tabs.executeScript(tab.id, {
+        "file": "contentScript.js"
+    });
+    // Send a message to the active tab
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+    });
 });
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+    
 });
